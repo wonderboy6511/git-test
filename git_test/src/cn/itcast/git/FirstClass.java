b5e405cd66868b20d69593780f8924d4223cec41
wonderboy <chezuopeng@qq.com>
2019-03-14 16:45:49 +0800

    把本地工程添加到git.

---
+++ git_test/src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git progect");
+        System.out.println("将项目保存到git");
+    }
+
+
+}
